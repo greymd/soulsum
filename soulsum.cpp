8cac654af773bb26f58e04c6c9b403ded4e09ae7
greymd <greengregson@gmail.com>
2017-11-23 18:03:55 +0900

    Add version feature

--- soulsum.cpp
+++ soulsum.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
+char const* version_string = "v1.5";
 int64_t getSoulNum (int64_t num);
 
 int64_t getSoulNum (int64_t num) {
-	int result = num % 9;
-	return result == 0 ? 9 : result;
+    int result = num % 9;
+    return result == 0 ? 9 : result;
 }
 
 int main(int argc, char const* argv[])
 {
-	int64_t num;
+    int64_t num;
 
-	if (argc >= 2) {
-		std::cout << getSoulNum (atoi(argv[1])) << std::endl;
-		return 0;
-	}
+    if (argc >= 2) {
+        if (strcmp("--version", argv[1]) == 0) {
+            std::cout << version_string << std::endl;
+        } else {
+            std::cout << getSoulNum (atoi(argv[1])) << std::endl;
+        }
+        return 0;
+    }
 
-	while (std::cin >> num) {
-		std::cout << getSoulNum (num) << std::endl;
-	}
-	return 0;
+    while (std::cin >> num) {
+        std::cout << getSoulNum (num) << std::endl;
+    }
+    return 0;
 }
